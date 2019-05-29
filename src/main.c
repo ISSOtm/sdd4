2632903bea2cd07979b044358c9800e50d48ec3f
ISSOtm <eldredhabert0@gmail.com>
2019-05-29 22:54:53 +0200

    Allow selecting any word for translation, and add more debugging output

--- src/main.c
+++ src/main.c
@@ -6,19 +6,21 @@
 
 int main(int argc, char * argv[]) {
     HashTable_t dictionary;
-    
-    if(argc < 2) {
-        printf("Usage:  %s path/to/language.file\n", argv[0]);
-    
+
+    if(argc < 3) {
+        printf("Usage:  %s path/to/language.file word_to_translate\n", argv[0]);
+
     } else {
         dictionary = dict_from_file(argv[1]);
         if(dictionary == NULL) {
             perror("Failed to open language file");
-        
+
         } else {
             printf("Average entry count: %f\n", average_entry_count(dictionary));
 
-            printf("\"hello\" gets translated to: %s\n", translate(dictionary, "hello"));
+            print_hashtable(dictionary);
+
+            printf("\"%s\" gets translated to: \"%s\"\n", argv[2], translate(dictionary, argv[2]));
 
             destroy_hashtable(dictionary);
         }

1a6ef5f6767fbb5020f5db0f6cf48279ab3d8067
ISSOtm <eldredhabert0@gmail.com>
2019-06-13 19:42:25 +0200

    Write doc comments

--- src/hashtable.h
+++ src/hashtable.h
@@ -15,23 +15,54 @@ typedef struct {
     unsigned nb_entries;
 } HashSubTable_t;
 
-/* The size of a has table is known, but declaring it as an array
+/* The size of a hash table is known, but declaring it as an array
  * creates a whole slew of problems when trying to allocate dynamically */
 typedef HashSubTable_t* HashTable_t;
 
 
+/**
+ * Creates a hash table and initializes it
+ * @return A pointer to the created hash table, or NULL in case of error
+ */
 HashTable_t create_hashtable();
 
+/**
+ * Destroys a hash table, properly freeing it and all pointers it contains
+ * @param table The table to be destroyed
+ */
 void destroy_hashtable(HashTable_t table);
 
 
-HashSubTableCell_t * insert_hashtable_entry(HashTable_t table, char const * word, char const * translation);
+/**
+ * Inserts a new entry into a hash table
+ * The strings passed to thins function are duplicated for internal use, so it's safe to `free` the arguments after this returns
+ * @param table The table to insert a new entry into
+ * @param The key where to insert the value to
+ * @param The value to insert
+ * @return A pointer to the inserted cell or NULL if an error occurred
+ */
+HashSubTableCell_t * insert_hashtable_entry(HashTable_t table, char const * key, char const * value);
 
-HashSubTableCell_t ** search_entry(const HashTable_t table, char const * word);
+/**
+ * Search for a value in a table by its key
+ * @param table The table to search into
+ * @param key The key to search for
+ * @return A pointer to the pointer to the cell holding the demanded key (useful for example for insertion or removal)
+ */
+HashSubTableCell_t ** search_entry(const HashTable_t table, char const * key);
 
 
+/**
+ * Prints a hashtable's contents in a "raw" way to stdout
+ * @param table The table to print
+ */
 void print_hashtable(const HashTable_t table);
 
+/**
+ * Returns the average number of entries per "hash key"
+ * @param table The table to compute the mean over
+ */
 float average_entry_count(const HashTable_t table);
+
 
 #endif
--- src/translation.h
+++ src/translation.h
@@ -1,12 +1,27 @@
-#ifndef TRADUCTION_H
-#define TRADUCTION_H
+#ifndef TRANSLATION_H
+#define TRANSLATION_H
 
 
 #include "hashtable.h"
 
 
+/**
+ * Translates a string from a given language into another
+ * @param dictionary The dictionary corressponding to the target language translation
+ * @param original The string to be translated (in the source language)
+ * @return A pointer to the translated string, or NULL if the source string isn't in the dictionary
+ * @warning THE RETURN POINTER MUST NOT BE MODIFIED! Use `strdup`, `strcpy` or `sprintf` if you need a writable copy. Modifying this could corrupt further searches, amongst other problems.
+ */
 char * translate(HashTable_t dictionary, char const * original);
 
+/**
+ * Reads a file and builds a dictionary from it
+ * The expected format is this: `source language expression;target language expression`
+ * Both expressions may contain spaces, tabs etc, just not semicolons
+ * No spaces should appear around the semicolon, though (TODO: improve this?)
+ * @param path The path to the file to be parsed
+ * @return The created dictionary, or NULL if something went wrong
+ */
 HashTable_t dict_from_file(char const * path);
 
 

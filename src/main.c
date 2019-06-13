92f7140f05bda6c01e7722d68bb9fcebd6c01e3b
KiiroNoKami <50236202+KiiroNoKami@users.noreply.github.com>
2019-06-13 21:09:04 +0200

    Update main.c
    Add list of cases in main.c

--- src/main.c
+++ src/main.c
@@ -6,7 +6,53 @@
 
 int main(int argc, char * argv[]) {
     HashTable_t dictionary;
+    HashTable_t test_dictionary;
+    HashTable_t empty_dictionary;
+    
+    char empty_text[] = "";
+    char words[] = {"student","book","Peter","bye","please wait"};
+    char unknown_word[] = "car";
+    
+    /*##########################*/
+    /*### LOADING FILE TESTS ###*/
+    /*##########################*/
+    
+    test_dictionary = dict_from_file("anglais.txt");
+    printf("\n=== English dictionary ===\n");
+    print_hashtable(test_dictionary);
+    printf("/n Average entry count for the English dictionary: %f/n",average_entry_count(test_dictionary));
+    
+    empty_dictionary = dict_from_file("empty.txt");
+    printf("\n=== Empty dictionary ===\n");
+    print_hashtable(empty_dictionary); 
+    printf("/n Average entry count for the empty dictionary: %f/n",average_entry_count(empty_dictionary));
+   
+    
+    /*#########################*/
+    /*### TRANSLATION TESTS ###*/
+    /*#########################*/
+    
+    printf("\n=== Translate list of words ===\n");
+    printf("text: %s\n", words);
+    printf("translation: %s\n",translate_multiple(test_dictionary, words, len(words)));
 
+    printf("\n=== Translate empty text ===\n");
+    printf("text: %s\n", empty_text);
+    printf("translation: %s\n",translate_multiple(test_dictionary, empty_text, len(empty_text)));
+
+    printf("\n=== Translate unknown word ===\n");
+    printf("text: %s\n", unknown_word);
+    printf("translation: %s\n",translate_multiple(test_dictionary, unknown_word, len(unknown_word)));
+    
+    printf("\n=== Translate with empty dictionary ===\n");
+    printf("text: %s\n", words);
+    printf("translation: %s\n",translate_multiple(empty_dictionary, words, len(words)));
+
+   
+    destroy_hashtable(test_dictionary);
+    destroy_hashtable(empty_dictionary);
+    
+    
     if(argc < 3) {
         printf("Usage:  %s path/to/language.file word_to_translate\n", argv[0]);
 
@@ -23,6 +69,7 @@ int main(int argc, char * argv[]) {
             printf("\"%s\" gets translated to: \"%s\"\n", argv[2], translate(dictionary, argv[2]));
 
             destroy_hashtable(dictionary);
+            
         }
     }
 
